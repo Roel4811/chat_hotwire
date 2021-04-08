d5df57926f5ccd3aceff84b627fe451cadda137f
Roel de Jong <roel@getrodeo.io>
2021-04-08 20:41:07 +0200

    fix actiontext with tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs'
+import '@hotwired/turbo-rails'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
 
 Rails.start()
 ActiveStorage.start()
 
-require("trix")
-require("@rails/actiontext")
-import "controllers"
+import 'trix'
+import '@rails/actiontext'
+import 'controllers'
+import '../css/actiontext.css'
 import '../css/application.css'
