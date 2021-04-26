ed1a54db74241ee8fcabfc3c5ec4963676999117
Roel de Jong <Roel4811@hotmail.com>
2021-04-26 12:07:19 +0200

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
     resources :messages
   end
 
-  resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
